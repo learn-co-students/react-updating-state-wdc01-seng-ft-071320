ca4698f6a9f8f0915b0cd1c8cbac109de8b4383c
solomonfy <fantahunsolomon@gmail.com>
2020-09-16 02:47:49 -0400

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -15,17 +15,29 @@ class ClickityClick extends Component {
   //   }
 
   handleClick = () => {
-    this.setState({
-      hasBeenClicked: true,
-    });
+    // passing an object to update the state
+    // this.setState(
+    //   {
+    //     hasBeenClicked: true,
+    //   },
+    //   () => console.log(this.state.hasBeenClicked)
+    // );
     // console.log(this.state.hasBeenClicked);
+    this.setState((previousState) => {
+      // using fun'n previousState to update the state
+      return {
+        hasBeenClicked: !previousState.hasBeenClicked,
+      };
+    });
   };
 
   render() {
     return (
       <div>
         <p>I have {this.state.hasBeenClicked ? null : "not"} been clicked!</p>
-        <button onClick={this.handleClick}>Click me</button>
+        <button onClick={this.handleClick}>
+          {this.state.hasBeenClicked ? "YES" : "NO"}
+        </button>
       </div>
     );
   }
